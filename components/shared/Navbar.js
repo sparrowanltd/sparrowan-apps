eceba80b2a27bf302c5df1eab7fe3aa18750877f
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-14 21:36:01 +0600

    navbar add

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from '../../public/sparrowan-logo.png';
 export default function Navbar() {
     return (
         <>
-           <h1>Navbar</h1>
+            <header className="bg-[#000000] px-20 text-white flex justify-between items-center nav_bar">
+                <Link href='/'> <Image src={logo} width="129px" height="53px" alt="Logo" placeholder="blur" /></Link >
+                <ul className="flex justify-between items-center lg:gap-10 xl:gap-16 ">
+                    <Link href='/'>Home</Link >
+                    <Link href='/service'>Service</Link >
+                    <Link href='/projects'>Projects</Link >
+                    <Link href='/protfolio'>Protfolio</Link >
+                    <Link href='/career'>Career</Link >
+                </ul>
+                <Link href='/contact-us'>Contact us</Link >
+            </header>
         </>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['cdn.example.com']
+  },
   reactStrictMode: true,
 }
 
