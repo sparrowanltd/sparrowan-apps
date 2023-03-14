8fcced38248680d8ae41d3025e68d88b371447ef
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-14 20:41:17 +0600

    first commit

---
+++ components/layout/Layout.js
@@ -0,0 +1,14 @@
+import Footer from "../shared/Footer";
+import Navbar from "../shared/Navbar";
+
+export default function Layout({ children }) {
+
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+
+    )
+}
