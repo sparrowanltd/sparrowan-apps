d153e09776d6b64468c2b0c9336882186f6815d3
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-15 10:25:31 +0600

    footer add

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
         <>
             <Navbar />
             {children}
-            {/* <Footer /> */}
+            <Footer />
         </>
 
     )
