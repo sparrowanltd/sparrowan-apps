58b469c4b187693cfc6947aa6f6566362fb0e20c
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-15 13:15:20 +0600

    carosuler add

--- components/shared/SwiperSlide.js
+++ components/shared/SwiperSlide.js
@@ -4,80 +4,47 @@ import { Navigation, Pagination, Autoplay } from 'swiper';
 import "swiper/css";
 import "swiper/css/pagination";
 import 'swiper/css/navigation';
+import Link from "next/link";
+import Image from "next/image";
 
-export default function SwiperSlideContent() {
+export default function SwiperSlideContent({ item, deretion, slideInfo }) {
     const progressCircle = useRef(null);
     const progressContent = useRef(null);
-    const onAutoplayTimeLeft = (s, time, progress) => {
+    const onAutoplayTimeLeft = (time, progress) => {
         progressCircle.current.style.setProperty('--progress', 1 - progress);
-        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}`;
     };
     return (
         <>
             <Swiper
-                slidesPerView={5}
-                spaceBetween={30}
+                slidesPerView={item}
                 autoplay={{
-                    delay: 2500,
+                    delay: deretion,
                     disableOnInteraction: false,
                 }}
                 pagination={{
                     clickable: true,
                 }}
-                modules={[Autoplay, Pagination, Navigation]}
+                modules={[Autoplay]}
                 onAutoplayTimeLeft={onAutoplayTimeLeft}
-                className="mySwiper "
+                className="mySwiper"
             >
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 1
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 2
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 3
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 4
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 5
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 6
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 7
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 8
-                    </div>
-                </SwiperSlide>
+                {
+                    slideInfo?.map(data => {
+                        return (
+                            <SwiperSlide className="">
+                                <Link href='/'>
+                                    <Image src={data?.icon} width="100" height="100" alt="imges" />
+                                </Link>
+                            </SwiperSlide>
+                        )
+                    })
+                }
 
-                <SwiperSlide>
-                    <div className="w-[300px] h-[300px] bg-red-300 rounded">
-                        slide 9
-                    </div>
-                </SwiperSlide>
-                <div className="autoplay-progress" slot="container-end">
+                <div className="autoplay-progress " slot="container-end">
 
-                    <span ref={progressCircle}></span>
-                    <span ref={progressContent}></span>
+                    <span ref={progressCircle} className="hidden "></span>
+                    <span ref={progressContent} className="hidden "></span>
                 </div>
 
             </Swiper>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,67 @@
-/* marius-masalar-CyFBmFEsytU-unsplash 1.png */
 import Button from "@/components/shared/button/Button";
 import icon from '../assets/Vector2.png';
+import xmVhugy5mnBZgI7ie from '../assets/1__xmVhugy5mnBZgI7ie295w.png';
+import AAA from '../assets/AAA-logo-2021-ENG-transparent.png';
+import iso from '../assets/iso-certified.png';
+import certificate from '../assets/certificate-logos-2-05.png';
+import naid from '../assets/naid.png';
+import unnamed from '../assets/unnamed.png';
 import Link from "next/link";
 import SwiperSlideContent from "@/components/shared/SwiperSlide";
 
 export default function Home() {
+
+  const slideInfo = [
+    {
+      link: "/",
+      icon: xmVhugy5mnBZgI7ie,
+    },
+    {
+      link: "/",
+      icon: AAA,
+    },
+    {
+      link: "/",
+      icon: iso,
+    },
+    {
+      link: "/",
+      icon: certificate,
+    },
+    {
+      link: "/",
+      icon: naid,
+    },
+    {
+      link: "/",
+      icon: unnamed,
+    },
+    {
+      link: "/",
+      icon: xmVhugy5mnBZgI7ie,
+    },
+    {
+      link: "/",
+      icon: AAA,
+    },
+    {
+      link: "/",
+      icon: iso,
+    },
+    {
+      link: "/",
+      icon: certificate,
+    },
+    {
+      link: "/",
+      icon: naid,
+    },
+    {
+      link: "/",
+      icon: unnamed,
+    },
+  ]
+
   return (
     <>
       <main className="banner_bg_imges">
@@ -28,7 +85,10 @@ export default function Home() {
           </div>
         </section>
       </main>
-      <SwiperSlideContent/>
+      <section className="p-20">
+        <SwiperSlideContent item={7} deretion={1000} slideInfo={slideInfo} />
+      </section>
+
     </>
   )
 }
