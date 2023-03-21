9b5a846269bc37d04eacda0593471d6c681f8060
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-21 21:53:14 +0600

    dfas

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,11 +3,36 @@ import Navbar from "../shared/Navbar";
 import styles from '../../styles/scrollbar.module.css'
 import "@fontsource/poppins";
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { useEffect } from "react";
 
 export default function Layout({ children }) {
+    useEffect(() => {
+
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "100356108528777");
+        chatbox.setAttribute("attribution", "biz_inbox");
+        window.fbAsyncInit = function () {
+            FB.init({
+                xfbml: true,
+                version: 'v16.0'
+            });
+        };
+
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    },[])
+
     return (
-        /* 100064843846173 */
         <>
+            <div id="fb-root"></div>
+
+            <div id="fb-customer-chat" class="fb-customerchat">
+            </div>
             <div style={{ fontFamily: "Poppins" }} className="overflow-hidden">
                 <Navbar />
                 {children}
