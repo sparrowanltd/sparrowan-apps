5f8b89512919872f6c3c012269db72e7488cf54c
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-21 22:20:50 +0600

    sd

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,18 +2,19 @@ import Footer from "../shared/Footer";
 import Navbar from "../shared/Navbar";
 import styles from '../../styles/scrollbar.module.css'
 import "@fontsource/poppins";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import MessengerChat from "@/utils/messengerChat";
 export default function Layout({ children }) {
     return (
         <>
             <div style={{ fontFamily: "Poppins" }} className="overflow-hidden">
+                <MessengerChat />
                 <Navbar />
                 {children}
                 <Footer />
-                <MessengerCustomerChat
+                {/* <MessengerCustomerChat
                     pageId="100356108528777"
                     appId="4953855674652215"
-                />
+                /> */}
             </div>
         </>
 
