06fba0f66c5176bed444117f88658590b45c0df3
Mosiur <mosiurislamwebdesign@gmail.com>
2023-03-31 15:55:09 +0600

    footer add

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -16,17 +16,17 @@ export default function Footer() {
     return (
         <>
             <footer
-                className="dark:bg-[#000000]  lg:text-left">
-                
+                className="bg-[#000000]  lg:text-left text-white">
+
                 <div className="px-5 md:px-20 py-10  md:text-left">
                     <div className="grid grid-cols-1 md:grid-cols-3 lg:flex justify-between">
                         <div className="">
                             <h1
                                 className="mb-4 flex items-center  font-semibold uppercase justify-start">
-                                <Link href='/'> <Image src={logo} className="w-100" width="" height="" alt="Logo"  /></Link >
+                                <Link href='/'> <Image src={logo} className="w-100" width="" height="" alt="Logo" /></Link >
                             </h1>
                             <div className="">
-                               {/*  <p
+                                {/*  <p
                                     className="  text-[#FF6B00]    font-semibold ">
                                     USA
                                 </p>
@@ -36,12 +36,13 @@ export default function Footer() {
                                     +1 (954) 449-5289
 
                                 </p>
+                                 */}
                                 <p
                                     className="mb-2  text-[#FF6B00]  font-semibold ">
-                                    UK Office
+                                    Japan Office
                                 </p>
-                                <p className="mb-2 text-sm">275 New N Rd Ste 3087, <br />London N1 7AA</p>
-                                <p className="mb-4 text-sm  ">+44 (204) 5577-0819</p> */}
+                                <p className="mb-2 text-sm">3-21-2Naka-ward, <br /> Yokohama-city, Kanagawa-prefecture, japan</p>
+                                <p className="mb-4 text-sm  ">+81 (045) 222-3232</p>
                                 <p
                                     className="mb-2  text-[#FF6B00]  font-semibold ">
                                     Dhaka Office
@@ -60,43 +61,43 @@ export default function Footer() {
 
                             <div className="mt-8">
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >About us</Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >  Our story </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     > Blog  </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >  Contact </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >  Support </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     > Payments  </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >  Career </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >  News </Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     > Foundation</Link >
                                 </p>
                                 <p className="mb-4">
-                                    <Link href="#" 
+                                    <Link href="#"
                                     >Site Map</Link >
                                 </p>
                             </div>
@@ -105,46 +106,46 @@ export default function Footer() {
                         <div>
                             <h1
                                 className="mb-4 mt-10 flex  text-xl font-semibold text-[#FF6B00] justify-start">
-                                Get In Touch
+                                Get in Touch
                             </h1>
                             <div className="mt-8">
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >contact@sparrowan.com</a >
                                 </p>
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >hr@sparrowan.com</a >
                                 </p>
                                 <br />
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >+88 017 85693445</a >
                                 </p>
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >skype: sparrowan</a >
                                 </p>
                                 <br />
                                 <p className="mb-4">
-                                    <a href="#!" className="text-[#FF6B00] dark:text-neutral-200"
+                                    <a href="#!" className="text-[#FF6B00] "
                                     >Opening Hour</a >
                                 </p>
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >Monday - Friday</a >
                                 </p>
                                 <p className="mb-4">
-                                    <a href="#!" className="text-[#FF6B00] dark:text-neutral-200"
+                                    <a href="#!" className="text-[#FF6B00] "
                                     >9:30 AM - 7:00 PM</a >
                                 </p>
                                 <p className="mb-4">
-                                    <a href="#!" 
+                                    <a href="#!"
                                     >Monday - Friday</a >
                                 </p>
                                 <p className="mb-4 flex">
                                     <input type='email' placeholder="Type your email" className="w-100 px-2 outline-none border-2 rounded-lg border-[#FF6B00] bg-transparent placeholder:text-sm" />
-                                    <Image src={Vector} className="w-[75px] bg-[#FF6B00] px-6 py-1 ml-[-20px] rounded-lg" width="" height="" alt="Logo"  />
+                                    <Image src={Vector} className="w-[75px] bg-[#FF6B00] px-6 py-1 ml-[-20px] rounded-lg" width="" height="" alt="Logo" />
                                 </p>
                             </div>
                         </div>
@@ -152,20 +153,20 @@ export default function Footer() {
                     {/*  */}
                     <hr className="my-6" />
                     <div className="md:flex justify-between items-center ">
-                        <p className="text-sm">© {year} – Sparrowan Limited | All Rights Reserved 
-                        <br />
-                        Terms of Services I Privacy Policy I Disclaimer
+                        <p className="text-sm">© {year} – Sparrowan Limited | All Rights Reserved
+                            <br />
+                            Terms of Services I Privacy Policy I Disclaimer
                         </p>
                         <ul className="mt-10 md:mt-10 flex justify-between items-center gap-2 md:gap-5  ">
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group1}  width="30px" height="auto" alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group2} width="30px" height="auto"  alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group3} width="30px" height="auto"  alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group4} width="30px" height="auto"  alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group5} width="30px" height="auto"  alt="Logo"  /></Link ></li>
-                            <li className=""><Link href='/'> <Image src={Line} width="30px" height="10px"  alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group1}  width="30px" height="auto" alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group2} width="30px" height="auto"  alt="Logo"  /></Link ></li>
-                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group3} width="30px" height="auto"  alt="Logo"  /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group1} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group2} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group3} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group4} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group5} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className=""><Link href='/'> <Image src={Line} width="30px" height="10px" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group1} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group2} width="30px" height="auto" alt="Logo" /></Link ></li>
+                            <li className="bg-black p-1 rounded-lg"><Link href='/'> <Image src={Group3} width="30px" height="auto" alt="Logo" /></Link ></li>
                         </ul>
 
                     </div>
